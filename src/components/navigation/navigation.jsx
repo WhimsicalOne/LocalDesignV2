855a268e9dd2bb735b80e169414f21008461d993
WhimsicalOne <dean@local-design.co.za>
2019-08-10 00:42:00 +0200

    Styled basic navigation and added Link component to them

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import Logo from "../../images/logo2.png";
+// Will add svg later...
 const Navigation = () => {
     return (
-        <nav>
-            <ul>
-                <li>One</li>
-                <li>Two</li>
-                <li>Three</li>
+        <nav className="navigation">
+            <div className="logo-container">
+                <img src={Logo} alt="Local-Design Logo" />
+            </div>
+            <ul className="navigation-container">
+                <li className="nav-links">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="nav-links">
+                    <Link to="/about">About</Link>
+                </li>
+                <li className="nav-links">
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li className="nav-links">
+                    <Link to="/services">Services</Link>
+                </li>
             </ul>
         </nav>
     );
