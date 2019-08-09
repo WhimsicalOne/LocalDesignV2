9115fee0c5c007f328632d37456462f43134a1aa
WhimsicalOne <dean@local-design.co.za>
2019-08-10 00:25:51 +0200

    Created base pages and navigation and added router config as well

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.scss";
 
+// All pages
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Services from "./pages/services/services";
+import Contact from "./pages/contact/contact";
+
+//Other stuff..
+import { Route } from "react-router-dom";
+
+// Components
+import Navigation from "./components/navigation/navigation";
+
 function App() {
     return (
         <div className="App">
-            <h1>Hello World!!</h1>
+            <Navigation />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/services" component={Services} />
         </div>
     );
 }
---
+++ src/pages/services/services.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Services extends Component {
+    state = {};
+    render() {
+        return <div>Services page</div>;
+    }
+}
+
+export default Services;
